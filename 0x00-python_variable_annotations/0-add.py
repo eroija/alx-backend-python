991417ca344d6320fb2103b1ee8d82ea05f0c3f4
Eric <eroija@gmail.com>
2024-08-01 10:48:28 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for task 0"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns their sum.
+
+    Args:
+        a (float): first float to add.
+        b (float): second float to add.
+
+    Returns:
+        float: The sum of the two floats.
+    """
+    return a + b
